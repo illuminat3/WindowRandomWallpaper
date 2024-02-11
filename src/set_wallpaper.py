cca43483c23a37eceeac11a53e325a5b0f315e55
Matthew <matthewrawson2@gmail.com>
2024-02-11 20:06:40 +0000

    Moved set_wallpaper.py into resources

--- src/set_wallpaper.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-import sys
-
-def set_wallpaper(image_path):
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
-
-if __name__ == "__main__":
-    image_path = sys.argv[1]
-    set_wallpaper(image_path)
